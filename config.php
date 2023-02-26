1452771d0dd9f8324b0deaa8c17c321476f38844
AnonCoderymous <100998097+AnonCoderymous@users.noreply.github.com>
2023-02-26 17:00:04 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'students');
+	define('DB_HOST', 'localhost'); //enter hostname
+	define('DB_USER', 'root'); //enter db user
+	define('DB_PASS', '');  //enter db password
+	define('DB_NAME', 'db_org'); //enter db name
 	$conn=mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 ?>
